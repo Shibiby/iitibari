172c01eb48f60008a99cf6480f28ff6ab69c2650
Shibiby <muhammadfeisal10.mf@gmail.com>
2020-12-14 13:54:33 +0300

    creating user api's

---
+++ config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+  connection: {
+    host: 'localhost',
+    user: 'shibiby',
+    password: 'Mjanaheri98',
+    database: 'iitibari_farm'
+  },
+  database: 'iitibari_farm',
+};
